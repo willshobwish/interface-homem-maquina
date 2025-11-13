2ecc985ec3d0b0f6f2cf6af08ec27d36f5db15f7
willshobwish <willianyos13@gmail.com>
2025-11-13 16:56:15 -0300

    Create: página de vendido com sucesso

---
+++ src/app/pages/vendido-sucesso/vendido-sucesso.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-vendido-sucesso',
+  imports: [RouterLink],
+  templateUrl: './vendido-sucesso.html',
+  styleUrl: './vendido-sucesso.scss',
+})
+export class VendidoSucesso {}
